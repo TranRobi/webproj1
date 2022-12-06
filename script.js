4386a231cc500ac877da71c9e437c92e6e1d9289
Tran Duy Dat <20d_trand@NYIRSZIKSZI.local>
2022-12-06 08:40:56 +0100

    as

---
+++ script.js
@@ -0,0 +1,17 @@
+let testsuly = Number(prompt("Testsúlya"))
+let etkezesek = Number(prompt("Étkezések száma"))
+let ml = Number(prompt("Megevett ml"))
+
+
+let testsuly_hat = testsuly / 6
+let etkezes = testsuly_hat / etkezesek
+let kellmeg = etkezes - ml
+
+document.write(`Testsúlya: ${testsuly}, etkezések száma: ${etkezesek}, Evett eddig mlban ${ml}, testsúly hatoda: ${testsuly_hat}, 1 étkezésre szánt mennyiség ${etkezes}, ennyi kellene még ${kellmeg} `)
+document.write("<br>")
+document.write(`Ennyi kell még ${etkezes+(kellmeg/(etkezesek-1))} a következő étkezésben`)
+
+if(etkezes < ml){
+    document.write("<br>")
+    document.write("Túl ette magát")
+}
